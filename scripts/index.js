252260c3d08a3c033e3ddd64f098f3e816e5a33f
Maxime Falcetta <maxime.falcetta@stagiairesmns.fr>
2024-07-21 06:27:00 +0200

    modified:   assets/css/style.css modified:   index.html new file:   scripts/index.js

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".header").classList.add("visible");
+    document.querySelector(".commands").classList.add("visible");
+  });
+  
